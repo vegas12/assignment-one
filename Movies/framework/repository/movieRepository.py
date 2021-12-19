cec698e2984fb6b3f1fdc84520aea0b984e7f075
vegas12 <burger_king89@hotmail.com>
2021-12-19 11:39:47 +0100

    Implement repository methods for Movies.
    Signed-off-by: vegas12 <burger_king89@hotmail.com>

--- Movies/framework/repository/movieRepository.py
+++ Movies/framework/repository/movieRepository.py
@@ -1,6 +1,32 @@
-from Movies.interface.abstractMovieRepository import AbstractMovieRepository
+from typing import List
+
+from Movies.Entity.Movie import Movie
+from Movies.framework.database.connection import Connection
 
 
-class MovieRepository(AbstractMovieRepository):
+class MovieRepository:
+    def __init__(self, connection: Connection ):
+        self.connection = connection
+
     def get_by_id(self, movie_id: int):
-        print("get_by_id")
+        self.connection.query("SELECT * FROM movies where movie_id = ?", [movie_id])
+
+    def get_all(self) -> List[Movie]:
+        results = self.connection.query("SELECT * FROM movies", []).fetchall()
+
+        movies_list: List[Movie] = []
+
+        for result in results:
+            movies_list.append(Movie(result[0], result[1]))
+
+        return movies_list
+
+    def create(self, movieDict) -> Movie:
+        self.connection.query("INSERT INTO movies(movie_name) VALUES (?)", [movieDict['name']])
+        self.connection.getLastRowId()
+        self.connection.commit()
+
+        return Movie(
+            self.connection.getLastRowId(),
+            movieDict['name']
+        )
